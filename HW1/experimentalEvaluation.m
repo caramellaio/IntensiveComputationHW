0bbae5c62201405b8d598adafc707e9fca78e0c0
caramellaio <albertobombardelli123@gmail.com>
2020-03-16 19:26:10 +0100

    Fix wrong sign

--- HW1/experimentalEvaluation.m
+++ HW1/experimentalEvaluation.m
@@ -1,10 +1,10 @@
-genRandomMatrix(10, 0.8, 100)
+genRandomMatrix(10, 0.95, 100)
 
 function M = genRandomMatrix(n, sparsity, ubound)
   M = zeros(n);
   for i = 1:n
     for j = 1:n
-      if rand(1) <= sparsity
+      if rand(1) > sparsity
         M(i, j) = rand(1,1) * ubound;
       end
     end
