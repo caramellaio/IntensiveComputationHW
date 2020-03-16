7840c056583f8471cc8640861655cadce4d250a0
caramellaio <albertobombardelli123@gmail.com>
2020-03-16 19:29:36 +0100

    Add CSC representation skeleton

---
+++ HW1/CSC.m
@@ -0,0 +1,16 @@
+function c = toCompactCSC(M)
+  A = nonzeros(M);
+  % continue here... 
+end
+
+function col = extractColCSC(M, j)
+end
+
+function row = extractRowCSC(M, j)
+end
+
+function b = mulCSC(A, B)
+end
+
+function M = toFullCSC(c)
+end
