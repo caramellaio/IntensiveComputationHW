1995b061a0d841330877c60aa5d5bdcf4704d575
caramellaio <albertobombardelli123@gmail.com>
2020-03-17 07:38:04 +0100

    Use ad-hoc function to gen "full" sparse matrix

--- HW1/experimentalEvaluation.m
+++ HW1/experimentalEvaluation.m
@@ -1,12 +1,13 @@
 genRandomMatrix(10, 0.95, 100)
 
 function M = genRandomMatrix(n, sparsity, ubound)
-  M = zeros(n);
+  %{M = zeros(n);
   for i = 1:n
     for j = 1:n
       if rand(1) > sparsity
         M(i, j) = rand(1,1) * ubound;
       end
     end
-  end
+  end%}
+  M = full(sprand(n,n, 1 - sparsity));
 end
