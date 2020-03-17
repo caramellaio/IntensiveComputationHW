1efb1a8906f48d513743d25ddd792d448cef4ec5
caramellaio <albertobombardelli123@gmail.com>
2020-03-17 22:14:41 +0100

    Implement column extraction

--- HW1/CSC.m
+++ HW1/CSC.m
@@ -28,10 +28,22 @@ function c = toCompactCSC(M)
   c = struct('A', A, 'I', I, 'J', J);
 end
 
-function col = extractColCSC(M, j)
+function col = extractColCSC(C, j)
+  % this works with the assumption of square matrix
+  n = length(C.J) -1;
+  col = zeros(n, 1);
+
+  range = C.J(j):C.J(j+1)-1;
+  col_nz = C.A(range);
+  i_idxs = C.I(range);
+
+  % assign non zero elements
+  for i = 1:length(i_idxs)
+    col(i_idxs(i)) = col_nz(i);
+  end
 end
 
-function row = extractRowCSC(M, j)
+function row = extractRowCSC(M, i)
 end
 
 function b = mulCSC(A, B)
