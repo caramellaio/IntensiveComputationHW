3defbea3d16047b418b9e41331ff6651e4c15ece
caramellaio <albertobombardelli123@gmail.com>
2020-03-17 22:14:17 +0100

    Minor fix

--- HW1/CSC.m
+++ HW1/CSC.m
@@ -1,5 +1,5 @@
 function funs = CSC
-  funs.toCompact=@toCompactCSCl
+  funs.toCompact=@toCompactCSC;
   funs.extractCol=@extractColCSC;
   funs.extractRow=@extractRowCSC;
   funs.mul=@mulCSC;
@@ -10,16 +10,16 @@ function c = toCompactCSC(M)
   % we might apply a stronger compression on the format
 
   % assume square matrix
-  n = size(M, 1)
+  n = size(M, 1);
 
-  [I_tmp, foo, A] = find(M)
+  [I_tmp, foo, A] = find(M);
 
   % find returns double precision matrixes for indexes too
   I = int32(I_tmp);
   J = zeros(n + 1, 1, 'int32');
 
   % get count of nnz elems in each column
-  nnz_J = sum(M~=0, 1);
+  nnz_J = sum(M~=0, 1)';
 
   % J is the cululative sum of the nnz elements of each column
   % we need to prepend once since cumsum otherwise each element would point to
